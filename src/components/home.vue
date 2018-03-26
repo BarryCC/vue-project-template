6a4c3f4917fbdf9cd380bb94c3439bfd9b091fcb
Your Name <643987053@qq.com>
2018-03-26 14:52:45 +0800

    all project

---
+++ src/components/home.vue
@@ -0,0 +1,25 @@
+
+<style scoped>
+</style>
+<style lang="scss">
+$color:red;
+.hello{
+  color: $color;
+}
+</style>
+
+<template>
+  <div class="hello">
+    this is a simple vue project!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
