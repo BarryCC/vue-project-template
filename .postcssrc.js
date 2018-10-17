5939f64ab699ec1bc0bf47d51c67c035ac1c2e13
BarryCC <643987053@qq.com>
2018-10-17 15:44:37 +0800

    user vm by postcss

--- .postcssrc.js
+++ .postcssrc.js
@@ -8,3 +8,36 @@ module.exports = {
     "autoprefixer": {}
   }
 }
+
+/************ 移动端VM（viewport width）适配，工具postcss ************** */
+// npm i postcss-aspect-ratio-mini postcss-px-to-viewport postcss-write-svg postcss-cssnext postcss-viewport-units cssnano --S
+// npm i cssnano-preset-advanced --save-dev
+
+// module.exports = {
+//   "plugins": {
+//     "postcss-import": {},
+//     "postcss-url": {},
+//     "postcss-write-svg": {
+//       "utf8": false
+//     },
+//     "postcss-cssnext": {},
+//     "postcss-px-to-viewport": {
+//       "viewportWidth": 375,
+//       "viewportHeight": 1334,
+//       "unitPrecision": 3,
+//       "viewportUnit": "vw",
+//       "selectorBlackList": [
+//         ".ignore",
+//         ".hairlines"
+//       ],
+//       "minPixelValue": 1,
+//       "mediaQuery": false
+//     },
+//     "postcss-viewport-units": {},
+//     "cssnano": {
+//       "preset": "advanced",
+//       "autoprefixer": false,
+//       "postcss-zindex": false
+//     }
+//   }
+// }
